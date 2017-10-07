33deaaf84ab0cfef3757f55734d5e2ec60db5460
Ken Kotch <hello@ken.uno>
2017-10-07 14:28:36 -0600

    knex returns object w/ 2 fist names, temp_id, owner_id & template_name== which is Bananas!

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,11 @@ const knex = require('../knex')
 
 // READ for User
 router.get('/', (req, res) => {
-  console.log('hi')
   // when authenticated, user can see owner, joined to schedule rendered on page
   knex('owner')
-    .select('first_name_1', 'first_name_2', 'template_id')
-    // .innerJoin('schedule', 'owner_id')
+    .select('first_name_1', 'first_name_2', 'template_id', 'schedule.owner_id', 'template.template_name')
+    .innerJoin('schedule', 'owner_id', 'owner.id')
+    .innerJoin('template', 'template.id', 'owner.template_id')
     .then((data) => {
       console.log(data)
       res.send(data)
