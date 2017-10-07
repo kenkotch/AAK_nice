ce3db8114ffd01f4f61da9b4333ad8695e02664a
Ken Kotch <hello@ken.uno>
2017-10-07 14:55:54 -0600

    returns appropriate data. loop to remove created_at and updated_at from schedule

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,15 @@ const knex = require('../knex')
 router.get('/', (req, res) => {
   // when authenticated, user can see owner, joined to schedule rendered on page
   knex('owner')
-    .select('first_name_1', 'first_name_2', 'template_id', 'schedule.owner_id', 'template.template_name')
+    .select('first_name_1', 'first_name_2', 'template.template_name', 'schedule.*')
+    // .where('schedule.owner_id', 'owner.id')
     .innerJoin('schedule', 'owner_id', 'owner.id')
     .innerJoin('template', 'template.id', 'owner.template_id')
     .then((data) => {
-      console.log(data)
+      for (let i = 0; i < data.length; i++) {
+        delete data[i].created_at
+        delete data[i].updated_at
+      }
       res.send(data)
     })
 })
