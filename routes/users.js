04c82fb1fd860e6e14799ec34f7e860895dcf646
Ken Kotch <hello@ken.uno>
2017-10-07 13:34:19 -0600

    resolve conflicts AGAIN

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,7 @@ const knex = require('../knex')
 router.get('/', (req, res) => {
   // when authenticated, user can see owner, joined to schedule rendered on page
   knex('owner')
-<<<<<<< HEAD
     .select('first_name_1', 'first_name_2', 'template_id')
-=======
-    .select('first_name_1', 'first_name_2' 'template_id')
->>>>>>> d907d9e3f5bb01389217dd441a042a945bbbc49f
     .innerJoin('schedule', 'owner_id')
 })
 
