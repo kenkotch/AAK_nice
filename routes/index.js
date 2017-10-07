b2fb21a9de8c4fb06d247ecb9e5875aa87de5bc8
Adam Neef <adneef@gmail.com>
2017-10-07 10:54:15 -0600

    added devstart

--- routes/index.js
+++ routes/index.js
@@ -2,34 +2,34 @@ const express = require('express')
 const router = express.Router()
 const knex = require('../knex')
 
-//GET all
-// router.get('/', (req, res, next) => {
-//   // code goes here
-// })
+// GET all lines of schedule
+router.get('/', (req, res, next) => {
 
-// R
-router.get('/:id', (req, res, next) => {
-  const id = Number(req.params.id)
-  // code goes here
+  knex('')
+
 })
-//CREATE a
+// CREATE a new schedule
 router.post('/', (req, res, next) => {
   const { item } = req.body
   // code goes here
 })
 
-// U
+// UPDATE an existing schedule, line by line
 router.patch('/:id', (req, res, next) => {
   const id = Number(req.params.id)
   const { item } = req.body
   // code goes here
 })
 
-// D
+// Future Expansion: UPDATE an existing schedule, all of it
+
+// DELETE an existing schedule, line by line
 router.delete('/:id', (req, res, next) => {
   const id = Number(req.params.id)
   // code goes here
 })
+
+// Future Expansion: DELETE an entire schedule
 
 
 module.exports = router
