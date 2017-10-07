55a38b8e1c51ba31198921edc076cca970ac9815
Ken Kotch <hello@ken.uno>
2017-10-07 14:04:03 -0600

    fixed synat errs in register.js

--- routes/register.js
+++ routes/register.js
@@ -3,18 +3,34 @@ const router = express.Router()
 const knex = require('../knex')
 const bcrypt = require('bcrypt')
 
-router.post('/', function (req, res, next) {
-  //now these must all be required fields in the request body/form input
-  const {username, email, password, first_name_1, last_name_1, first_name_2, last_name_2} = req.body
+router.post('/', (req, res, next) => {
+  // now these must all be required fields in the request body/form input
+  const {
+    username,
+    email,
+    password,
+    first_name_1,
+    last_name_1,
+    first_name_2,
+    last_name_2
+  } = req.body
 
-  bcrypt.hash(password, 5, function(err, hash) {
-
+  bcrypt.hash(password, 5, (err, hash) => {
     knex('owner')
-      .insert({username: username, email: email, hashed_password: hash, first_name_1: first_name_1, last_name_1: last_name_1, first_name_2: first_name_2, last_name_2: last_name_2})
-      .then(() =>{
+      .insert({
+        username,
+        email,
+        hashed_password: hash,
+        first_name_1,
+        last_name_1,
+        first_name_2,
+        last_name_2
+      })
+      .then(() => {
         res.send(200)
       })
   })
-  .catch((err) => next(err))
-  }
+    .catch((err) => {
+      next(err)
+    })
 })
