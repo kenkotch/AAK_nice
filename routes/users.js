802b2d321fbc4857d244151ed289b54c93ea03dc
Ken Kotch <hello@ken.uno>
2017-10-07 13:55:23 -0600

    testing

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,15 @@ const knex = require('../knex')
 
 // READ for User
 router.get('/', (req, res) => {
+  console.log('hi')
   // when authenticated, user can see owner, joined to schedule rendered on page
   knex('owner')
     .select('first_name_1', 'first_name_2', 'template_id')
-    .innerJoin('schedule', 'owner_id')
+    // .innerJoin('schedule', 'owner_id')
+    .then((data) => {
+      console.log(data)
+      res.send(data)
+    })
 })
 
 
