4bb62da4e930faf71b9c1fdf364e0b9131a73311
Adam Neef <adneef@gmail.com>
2017-10-08 11:35:52 -0600

    updated register to reflect database changes - wedding date

--- routes/register.js
+++ routes/register.js
@@ -11,10 +11,11 @@ router.post('/', (req, res, next) => {
     first_name_1,
     last_name_1,
     first_name_2,
-    last_name_2
+    last_name_2,
+    wedding_date
   } = req.body
 
-  if(!email || !password || !first_name_1 || !last_name_2 || !first_name_2 || !last_name_2) {
+  if(!email || !password || !first_name_1 || !last_name_2 || !first_name_2 || !last_name_2 || !wedding_date) {
     res.status(400)
     res.send('Please complete all fields')
     return
@@ -28,7 +29,8 @@ router.post('/', (req, res, next) => {
         first_name_1,
         last_name_1,
         first_name_2,
-        last_name_2
+        last_name_2,
+        wedding_date
       })
       .then(() => {
         return res.sendStatus(200)
