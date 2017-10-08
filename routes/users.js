d4002a62701e28fc8ecc0aa24759aef76662828a
Ken Kotch <hello@ken.uno>
2017-10-08 12:54:15 -0600

    getting data from users.js to users.ejs! now we can loop.

--- routes/users.js
+++ routes/users.js
@@ -3,39 +3,54 @@ const router = express.Router()
 const knex = require('../knex')
 
 // READ for User
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   let id = 1 // id will eventually come from cookie
+
+  let fName1
+  let fName2
+  let wedDate
+
+  // need to loop through these in ejs to show all of schedule
+  let scheduleTime
+  let scheduleItem
+  let scheduleDescripton
+  let newObj
+
   knex('owner')
     .select('first_name_1', 'first_name_2', 'wedding_date', 'template.template_name', 'schedule.*')
     .where('owner.id', id)
     .innerJoin('schedule', 'owner_id', 'owner.id')
     .innerJoin('template', 'template.id', 'owner.template_id')
     .then((data) => {
+      // console.log('data', data)
+      // console.log('length', data.length)
+      fName1 = data[0].first_name_1
+      fName2 = data[0].first_name_2
+      wedDate = data[0].wedding_date.toString().slice(0, 15)
+
       for (let i = 0; i < data.length; i++) {
         delete data[i].created_at
         delete data[i].updated_at
-        var fName1 = data[0].first_name_1
-        var fName2 = data[0].first_name_2
-        var wedDate = data[0].wedding_date.toString().slice(0, 15)
-
-        // need to loop through these in ejs to show all of schedule
-        var scheduleTime = data[i].time
-        var scheduleItem = data[i].item
-        var scheduleDescripton = data[i].description
-
-        console.log(scheduleItem)
       }
+
+        // scheduleTime = data[i].time
+        // scheduleItem = data[i].item
+        // scheduleDescripton = data[i].description
+        newObj = data
       res.render('./users', {
-        fName1,
-        fName2,
-        wedDate,
-        scheduleTime,
-        scheduleItem,
-        scheduleDescripton,
+        // fName1,
+        // fName2,
+        // wedDate,
+        // scheduleTime,
+        // scheduleItem,
+        // scheduleDescripton,
+        data,
         _layoutFile: 'layout.ejs'
       })
+      console.log(data)
     })
-})
+    .catch((err) => next(err))
 
+})
 
 module.exports = router
