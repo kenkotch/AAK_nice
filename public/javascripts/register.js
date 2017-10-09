9156724978e722a3ed869dbc66229ff84db462b5
Adam Neef <adneef@gmail.com>
2017-10-09 12:34:54 -0600

    update

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,19 +1,23 @@
 $(document).ready(() => {
   console.log('ready!')
 
-  $('#newOwner').submit((event) => {
-    event.preventDefault()
+  if (document.location.href.match(/register$/)) {
+    console.log('matching: register page')
 
-    let data = $('#newOwner').serialize()
-    console.log(data)
+    $('#newOwner').submit((event) => {
+      event.preventDefault()
 
-    $.post("/register", data, null, 'json').then((data) => {
-      console.log('Input data', data)
-      document.location = '/register'
+      let data = $('#newOwner').serialize()
+      console.log(data)
+
+      $.post("/register", data, null, 'json').then((data) => {
+        console.log('Input data', data)
+        document.location = '/register'
+      })
+      .fail((err) => {
+        $('#errorMessage').html(`<div>${err.responseText}</div>`)
+      })
     })
-    .fail((err) => {
-      $('#errorMessage').html(`<div>${err.responseText}</div>`)
-    })
-  })
+  }
 
 })
