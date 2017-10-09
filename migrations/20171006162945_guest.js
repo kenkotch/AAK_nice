00f79ffdf035a9e85f7a740c95b67d1cab8c1a75
Adam <Adam@Adams-MacBook-Pro-3.local>
2017-10-09 13:44:41 -0600

    added the profile EJS and route

--- migrations/20171006162945_guest.js
+++ migrations/20171006162945_guest.js
@@ -7,7 +7,7 @@ exports.up = (knex, Promise) => {
       .defaultTo('')
       .unique()
 
-    table.specificType('hashed_password', 'CHAR(60)')
+    table.varchar('password', 63)
       .notNullable()
 
     table.integer('owner_id')
--- routes/login.js
+++ routes/login.js
@@ -6,13 +6,18 @@ const jwt = require('jsonwebtoken')
 const secret = process.env.JWT_KEY
 
 router.get('/', (req, res, next) => {
-  res.render('login', {_layoutFile: 'layout.ejs'})
+  res.render('login', {
+    _layoutFile: 'layout.ejs'
+  })
 })
 
-router.post('/', function (req, res, next) {
-  const { email, password } = req.body
+router.post('/', function(req, res, next) {
+  const {
+    email,
+    password
+  } = req.body
 
-  if(!email || !password){
+  if (!email || !password) {
     res.status(400)
     res.send('Bad email or password')
     return
@@ -23,30 +28,35 @@ router.post('/', function (req, res, next) {
     .first()
     .then((data) => {
       let match = bcrypt.compare(password, data.hashed_password)
-        if(!match) {
-          res.sendStatus(404)
-          return
-        }
-        const token = jwt.sign({ownerId: data.id}, secret)
+      if (!match) {
+        res.sendStatus(404)
+        return
+      }
+      const token = jwt.sign({
+        ownerId: data.id
+      }, secret)
 
-        res.cookie('token', token,
-      {httpOnly: true})
+      res.cookie('token', token, {
+        httpOnly: true
+      })
       console.log(res.cookie)
       res.status(200)
-      res.render('login', {_layoutFile: 'layout.ejs'})
+      res.render('login', {
+        _layoutFile: 'layout.ejs'
+      })
     })
     .catch((err) => next(err))
 })
 
 router.get('/', (req, res, next) => {
   jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-    if(err){
-      return res.send(false)
-    }
-    res.render('login')
-    res.send(true)
-  })
-  .catch((err) => next(err))
+      if (err) {
+        return res.send(false)
+      }
+      res.render('login')
+      res.send(true)
+    })
+    .catch((err) => next(err))
 })
 
 router.delete('/', (req, res, next) => {
