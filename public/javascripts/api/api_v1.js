437ff68a1519c42461b0346786a6c1e2817cb42d
Ken Kotch <hello@ken.uno>
2017-10-10 13:09:49 -0600

    auth fixes

--- public/javascripts/api/api_v1.js
+++ public/javascripts/api/api_v1.js
@@ -1,42 +1,42 @@
 $(document).ready(() => {
 
-    // called on DELETE/EDIT success- reloads only table
-    function getAndRenderSchedule() {
-      $.ajax({
-        method: 'GET',
-        url: '/myschedule',
-        success: 'success'
-      })
-    }
-
-    // DELETE
-    $("#schedule .delete").click((e) => {
-      e.preventDefault()
-      console.log(`delete ${e.target.id}`)
-      $.ajax({
-        url: `/myschedule/${e.target.id}`,
-        method: "DELETE",
-        success: getAndRenderSchedule()
-      })
-        .then((data) => {
-          $(e.target).closest('tr').hide()
-        })
+  // called on DELETE/EDIT success- reloads only table
+  function getAndRenderSchedule() {
+    $.ajax({
+      method: 'GET',
+      url: '/myschedule',
+      success: 'success'
     })
+  }
 
-    // UPDATE
-    $("#editForm").submit((e) => {
-      e.preventDefault()
-      console.log('form submit')
-      let id = $('input[name="id"]').val()
-
-      $.ajax({
-        url: `/myschedule/${id}`,
-        method: "PATCH",
-        data: $('#editForm').serialize(),
-        success: (res) => {
-          console.log(res)
-          window.location = '/myschedule'
-        }
-      })
+  // DELETE
+  $("#schedule .delete").click((e) => {
+    e.preventDefault()
+    console.log(`delete ${e.target.id}`)
+    $.ajax({
+      url: `/myschedule/${e.target.id}`,
+      method: "DELETE",
+      success: getAndRenderSchedule()
     })
+      .then((data) => {
+        $(e.target).closest('tr').hide()
+      })
+  })
+
+  // UPDATE
+  $("#editForm").submit((e) => {
+    e.preventDefault()
+    console.log('form submit')
+    let id = $('input[name="id"]').val()
+
+    $.ajax({
+      url: `/myschedule/${id}`,
+      method: "PATCH",
+      data: $('#editForm').serialize(),
+      success: (res) => {
+        console.log(res)
+        window.location = '/myschedule'
+      }
+    })
+  })
 })
