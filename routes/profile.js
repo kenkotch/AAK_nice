8ecb908efcfa91d57b8288e2656038df8e7ef79f
Adam <Adam@Adams-MacBook-Pro-3.local>
2017-10-11 13:41:52 -0600

    get route works and need to add hashing to password for post route

--- routes/profile.js
+++ routes/profile.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const knex = require('../knex')
 const jwt = require('jsonwebtoken')
 const boom = require('boom')
-
-let guest_profile
+const secret = process.env.JWT_KEY
+let role
+// let guest_profile
 // RENDERS EVERYTHING FROM THIS OWNER'S SPECIFIC PROFILE
 
 const auth = (req, res, next) => {
@@ -33,22 +34,33 @@ const checkRole = (req, res, next) => {
 //   res.render('profile', { guest_profile, _layoutFile: 'layout.ejs' })
 // })
 //
-router.get('/:id', auth, checkRole, (req, res, next) => {
-  const id = Number(req.params.id)
-  knex('account')
-    .select('id', 'email', 'first_name_1', 'last_name_1', 'first_name_2', 'last_name_2', 'cover_url', 'created_at', 'updated_at')
-    .orderBy('id')
-    .where('id', id)
-    .then((profile) => {
-      if (profile.length < 1) {
-        return res.sendStatus(404)
-      }
-
-      res.setHeader('Content-Type', 'application/json')
-      res.send(JSON.stringify(profile[0]))
+router.get('/', auth, checkRole, (req, res, next) => {
+  // const id = req.params.id
+  let id = req.claim
+  console.log("id is:", id)
+  if (typeof id !== 'undefined') {
+    knex('account')
+      .select()
+      .where('id', id)
+      .first()
+      .then((data) => {
+        res.render('profile', {
+          title: `something is working at id ${id}`,
+          id,
+          first_name_1: data.first_name_1,
+          first_name_2: data.first_name_2,
+          last_name_1: data.last_name_1,
+          last_name_2: data.last_name_2,
+          wedding_date: data.wedding_date,
+          _layoutFile: 'layout.ejs'
+        })
+      })
+  } else {
+    res.status(500)
+    res.render('error', {
+      message: 'something went wrong'
     })
-    .catch((err) => next(err))
-
+  }
   // res.render('profile', {
   //   guest_profile,
   //   _layoutFile: 'layout.ejs'
@@ -57,128 +69,123 @@ router.get('/:id', auth, checkRole, (req, res, next) => {
 
 
 // HANDLES CREATION OF GUEST USERNAME AND PASSWORD - ADAM M.
-// router.post('/', (req, res, next) => {
-//   // const { item } = req.body
-//   jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-//     if (err) {
-//       return res.send(false)
-//     }
-//     let id = payload.ownerId
-//     console.log(id)
+router.post('/', auth, checkRole, (req, res, next) => {
+  // const { item } = req.body
+  let id = req.claim
+  if (!req.body.username || req.body.username.trim() === '') {
+    res.status(500)
+    res.render('error', {
+      message: 'Username cannot be blank'
+    })
+  } else if (!req.body.password || req.body.password.trim() === '') {
+    res.status(500)
+    res.render('error', {
+      message: 'Password cannot be blank'
+    })
+  } else {
+    knex('account')
+      .insert({
+        username: req.body.username,
+        hashed_password: req.body.password,
+        account_id: id,
+        role: 3
+      }, '*')
+      .then((data) => {
+        guest_profile = data
+        res.status(200)
+        // res.send(guest_profile[0])
+        res.redirect('/profile')
+      })
+      .catch((err) => next(err))
+  }
+})
+
+
+router.patch('/:id', (req, res, next) => {
+  const id = Number(req.params.id)
+
+  if (Number.isNaN(id)) {
+    return next()
+  }
+
+  knex('acount')
+    .where('id', id)
+    .then((rows) => {
+      if (!rows) {
+        throw boom.create(404, 'Not Found')
+      }
+
+      const {
+        email,
+        password,
+        first_name_1,
+        last_name_1,
+        first_name_2,
+        last_name_2,
+        wedding_date
+      } = req.body
+      const updateRow = {}
+      if (email) {
+        updateRow.email = email
+      }
+
+      if (password) {
+        updateRow.password = password
+      }
+
+      if (first_name_1) {
+        updateRow.first_name_1 = first_name_1
+      }
+
+      if (last_name_1) {
+        updateRow.last_name_1 = last_name_1
+      }
+
+      if (first_name_2) {
+        updateRow.first_name_2 = first_name_2
+      }
+
+      if (first_name_2) {
+        updateRow.first_name_2 = first_name_2
+      }
+
+      if (wedding_date) {
+        updateRow.wedding_date = wedding_date
+      }
+
+      knex('account')
+        .update(updateRow, '*')
+        .where('id', id)
+        .then((row) => {
+          res.send(row[0])
+        })
+    })
+    .catch((err) => next(err))
+})
 //
-//     if (!req.body.username || req.body.username.trim() === '') {
-//       res.status(500)
-//       res.render('error', {
-//         message: 'Username cannot be blank'
-//       })
-//     } else if (!req.body.password || req.body.password.trim() === '') {
-//       res.status(500)
-//       res.render('error', {
-//         message: 'Password cannot be blank'
-//       })
-//     } else {
-//       knex('guest')
-//         .insert({
-//           username: req.body.username,
-//           password: req.body.password,
-//           owner_id: id
-//         }, '*')
-//         .then((data) => {
-//           guest_profile = data
-//           res.status(200)
-//           // res.send(guest_profile[0])
-//           res.redirect('/profile')
-//         })
-//         .catch((err) => next(err))
-//     }
-//   })
-// })
-//
-// router.patch('/:id', (req, res, next) => {
-//   const id = Number(req.params.id)
-//
-//   if (Number.isNaN(id)) {
-//     return next()
-//   }
-//
-//   knex('acount')
-//     .where('id', id)
-//     .then((rows) => {
-//       if (!rows) {
-//         throw boom.create(404, 'Not Found')
-//       }
-//
-//       const {
-//         email,
-//         password,
-//         first_name_1,
-//         last_name_1,
-//         first_name_2,
-//         last_name_2,
-//         wedding_date
-//       } = req.body
-//       const updateRow = {}
-//       if (email) {
-//         updateRow.email = email
-//       }
-//
-//       if (password) {
-//         updateRow.password = password
-//       }
-//
-//       if (first_name_1) {
-//         updateRow.first_name_1 = first_name_1
-//       }
-//
-//       if (last_name_1) {
-//         updateRow.last_name_1 = last_name_1
-//       }
-//
-//       if (first_name_2) {
-//         updateRow.first_name_2 = first_name_2
-//       }
-//
-//       if (first_name_2) {
-//         updateRow.first_name_2 = first_name_2
-//       }
-//
-//       if (wedding_date) {
-//         updateRow.wedding_date = wedding_date
-//       }
-//
-//       knex('account')
-//         .update(updateRow, '*')
-//         .where('id', id)
-//         .then((row) => {
-//           res.send(row[0])
-//         })
-//     })
-//     .catch((err) => next(err))
-// })
-// //
-// router.delete('/:id', (req, res, next) => {
-//   const id = Number(req.params.id)
-//   if (Number.isNaN(id)) {
-//     return next()
-//   }
-//   let event
-//   knex('account')
-//     .where('id', id)
-//     .then((row) => {
-//       if (!row) {
-//         throw boom.create(404, 'Not Found')
-//       }
-//       event = row
-//
-//       return knex('account')
-//         .del()
-//         .where('id', id)
-//     })
-//     .then(() => {
-//       delete event.id
-//       res.send(event)
-//     })
-//     .catch((err) => next(err))
-// })
-//
+router.delete('/:id', (req, res, next) => {
+  const id = Number(req.params.id)
+  if (Number.isNaN(id)) {
+    return next()
+  }
+  let event
+  knex('account')
+    .where('id', id)
+    .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Not Found')
+      }
+      event = row
+
+      return knex('account')
+        .del()
+        .where('id', id)
+    })
+    .then(() => {
+      delete event.id
+      res.send(event)
+    })
+    .catch((err) => next(err))
+})
+
 module.exports = router
